10deb93c518d100ed0b751442b345b595633696d
Sarapezzotta20 <sarapezzotta20@hotmail.it>
2024-10-28 13:31:21 +1100

    first commit

---
+++ api/index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+// const Post = require("./models/Post");
+const bcrypt = require("bcryptjs");
+const app = express();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
+const salt = bcrypt.genSaltSync(10);
+const secret = "asdfe45we45w345wegw345werjktjwertkj";
+
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.connect(
+  "mongodb+srv://sarapezzotta20:Leonessa2010!@cluster0.urizw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    // logged in
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json({
+        id: userDoc._id,
+        username,
+      });
+    });
+  } else {
+    res.status(400).json("wrong credentials");
+  }
+});
+
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json("ok");
+});
+
+// app.listen(4000);
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ client/src/Post.js
@@ -0,0 +1,30 @@
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="image">
+        <img
+          src="https://www.rainews.it/cropgd/806x453/dl/img/2024/10/12/1728746896648_sinnerdjokovic.jpg"
+          alt="Sinner immagine"
+        />
+      </div>
+      <div className="texts">
+        <h2>
+          Tennis, finale Masters 1000 di Shanghai: Sinner-Djokovic per fare la
+          storia
+        </h2>
+        <p className="info">
+          <a href="/" className="author">
+            David Smith
+          </a>
+          <time>2024-10-13 17:02</time>
+        </p>
+        <p className="summary">
+          Un 2024 da incorniciare per l'altoatesino che chiude la stagione da
+          n°1 al mondo. Il serbo in campo per conquistare la centesima vittoria
+          Atp, meglio di lui nell'era Open soltanto lo svizzero Roger Federer
+          (103) e lo statunitense Jimmy Connors (109)
+        </p>
+      </div>
+    </div>
+  );
+}
